f01e72338279c4c8710d864232f0c971566a47b2
lysocheaj <lysochea@gmail.com>
2022-06-08 15:17:28 +0700

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return <div>
+      <h1>My Todo</h1>
+      <div className='card'>
+        <h2>Title</h2>
+        <div className='actions'>
+          <button className='btn'>Delete</button>
+        </div>
+      </div>
+  </div>;
+}
+export default App;
