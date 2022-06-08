b02302a101e9b2e8aa959c53ee5d91a95273c994
lysocheaj <lysochea@gmail.com>
2022-06-08 22:51:27 +0700

    add modal backdrop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Todo from './components/Todo';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return <div>
       <h1>My Todo</h1>
-      <Todo />
+      <Todo text='Learn React'/>
+      <Todo text='Learn NextJs'/>
+      <Todo text='Learn Angular'/>
+
+      <Modal />
+      <Backdrop />
   </div>
 }
 export default App;
